7b748dbfd39f3b123e69eba52cfb9b2a1c0d40db
86gumball <lucasruben2000@gmail.com>
2019-03-30 17:59:38 +0000

    pergunta51.c done (correta)

---
+++ pergunta51.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "listas.h"
+
+LInt newLInt (int v, LInt t){
+    LInt new = (LInt) malloc (sizeof (struct lligada));
+    
+    if (new!=NULL) {
+        new->valor = v;
+        new->prox  = t;
+    }
+    return new;
+}
+
+int length (LInt l){
+    int i;
+    for (i = 0; &l->valor != NULL; i++){
+    	l = l->prox;
+    }
+    return i;
+}
