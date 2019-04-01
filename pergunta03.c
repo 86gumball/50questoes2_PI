1943c9cd1813fe2469a596c7d08f9f1d69b84dea
86gumball <lucasruben2000@gmail.com>
2019-04-01 20:03:58 +0100

    teste pergunta03.c adicionado

--- pergunta03.c
+++ pergunta03.c
@@ -4,3 +4,16 @@ void imprimeL (LInt head) {
 		head = head->prox;
 	} 
 }
+
+/* Teste usado
+int main(){
+    LInt new1 = newLInt(5, NULL);
+    LInt new2 = newLInt(9, new1);
+    LInt new3 = newLInt(2, new2);
+    LInt new4 = newLInt(0, new3);
+    LInt new5 = newLInt(3, new4);
+    LInt new6 = newLInt(1, new5);
+    imprimeL(new6);
+    return 0;
+}
+*/
