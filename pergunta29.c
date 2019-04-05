68445f811b0df12b9d647382f43abafc7dd35200
86gumball <lucasruben2000@gmail.com>
2019-04-05 01:15:00 +0100

    pergunta29.c - correta

---
+++ pergunta29.c
@@ -0,0 +1,14 @@
+#include "abin.h"
+
+/* Copia-se o valor do nodo para a nova arvore e chama-se a função recursivamente
+   aos ramos, o caso de paragem é quando a função recebe um ramo nulo*/
+ABin cloneAB (ABin root) {
+	ABin new = NULL;
+	if (root != NULL){
+    	new = (ABin) malloc (sizeof (struct nodo));
+    	new->valor = root->valor;
+    	new->esq = cloneAB (root->esq);
+    	new->dir = cloneAB (root->dir);
+	}
+    return abin;
+}
