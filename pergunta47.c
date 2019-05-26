72b11b05d05b72be7fe380824e508624e2a7b233
86gumball <lucasruben2000@gmail.com>
2019-05-26 19:50:11 +0100

    pergunta47 - versao não recursiva

--- pergunta47.c
+++ pergunta47.c
@@ -1,14 +1,9 @@
 #include "abin.h"
 
 int maiorAB (ABin root) {
-	/* Se o nodo não tiver nenhum ramo à direita, significa que contem o maior valor
-	   da árvore binária de procura */
-	if (root->dir == NULL){
-		return root->valor;
-		
-	/* Caso contrário, vai-se chamando a função recursivamente ao ramo direito até
-	   chegar ao valor mais alto */
-	} else {
-		maiorAB(root->dir);
-	} 
+	/* Procura o ultimo nodo à direita e devolve o seu valor no final*/	
+	while(root->dir != NULL){
+		root = root->dir;
+	}
+	return root->valor;
 }
